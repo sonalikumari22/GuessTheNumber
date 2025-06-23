1e0422867e25ac0decd30f90c12ae2d787ead9ba
sonalikumari22 <dasswty24@gmail.com>
2025-06-23 19:02:24 +0530

    first commit

---
+++ GuessTheNumber.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class GuessTheNumber {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+Random rand = new Random();
+
+int n=100;
+int numberToGuess=rand.nextInt(n)+1;
+int userGuess=0;
+int attempts=0;
+
+System.out.println("welcome to guess the number game!");
+System.out.println("I have choosen a number for you can u guess it?");
+
+while (userGuess != numberToGuess) {
+    System.out.println("enter ur guess");
+    userGuess= sc.nextInt();
+    attempts++;
+
+    if(userGuess<numberToGuess){
+        System.out.println("too low! Try again");
+    }
+    else if (userGuess>numberToGuess) {
+        System.out.println("too High! Try again");
+    }
+    else{
+        System.out.println("correct ! you guessed it in "+ attempts + "attempts");
+    }
+}
+  sc.close();
+}
+}
